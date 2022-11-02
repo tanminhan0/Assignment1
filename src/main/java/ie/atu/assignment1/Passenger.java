b3ec2ba0c444ab6504e384a189a9a916f11e91fa
Tan Min Han <g00377211@atu.ie>
2022-11-02 16:35:33 +0000

    Created test package for the 5 instance of Passenger class

--- src/main/java/ie/atu/assignment1/Passenger.java
+++ src/main/java/ie/atu/assignment1/Passenger.java
@@ -1,4 +1,89 @@
-package ie.atu.assignment1;
+package ie.atu.Assignment1;
 
 public class Passenger {
+    private String title, name, id, phone;
+    private int age;
+
+
+    public Passenger(String title, String name, String id, String phone, int age){
+        this.title = title;
+        this.name =name;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setTitle(String title) {
+        //Check Title
+        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Ms")){
+            this.title = title;
+        }
+        else{
+            throw new IllegalArgumentException("This is not a valid title");
+        }
+
+    }
+
+    public void setName(String name) {
+        //Check name length
+        if(name.length() >= 3){
+            this.name = name;
+        }
+        else{
+            throw new IllegalArgumentException("Name must be greater than 3 characters");
+        }
+
+    }
+
+    public void setId(String id) {
+        //Check ID length
+        if(id.length() >= 10){
+            this.id = id;
+        }
+        else{
+            throw new IllegalArgumentException("ID length must be greater than 10 characters");
+        }
+    }
+
+    public void setPhone(String phone) {
+        //Check Phone Numbers length
+        if(phone.length() >= 7){
+            this.phone = phone;
+        }
+        else{
+            throw new IllegalArgumentException("Phone must be greater than 7 numbers");
+        }
+    }
+
+    public void setAge(int age) {
+        //Check Age eligibility to fly
+        if(age >= 16){
+            this.age = age;
+        }
+        else{
+            throw new IllegalArgumentException("Age below 16 is too young to fly");
+        }
+    }
 }
--- src/test/java/ie/atu/assignment1/PassengerTest.java
+++ src/test/java/ie/atu/assignment1/PassengerTest.java
@@ -1,15 +1,54 @@
-package ie.atu.assignment1;
+package ie.atu.Assignment1;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
+     Passenger myPassenger;
+
 
     @BeforeEach
     void setUp() {
+        myPassenger = new Passenger(null,null,null,null,0);
     }
+
+    @Test
+    void testTitle(){
+        myPassenger.setTitle("Mr");
+        assertEquals("Mr", myPassenger.getTitle());
+    }
+
+    @Test
+    void testName(){
+        myPassenger.setName("Tan");
+        assertEquals("Tan", myPassenger.getName());
+    }
+
+    @Test
+    void testId(){
+        myPassenger.setId("0037721199");
+        assertEquals("0037721199", myPassenger.getId());
+    }
+
+    @Test
+    void testPhone(){
+        myPassenger.setPhone("0833337038");
+        assertEquals("0833337038", myPassenger.getPhone());
+    }
+
+    @Test
+    void testAge(){
+        myPassenger.setAge(22);
+        assertEquals(22, myPassenger.getAge());
+    }
+
+//    @Test
+//    void testConstructor(){
+//        assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "James", "0012834901", "0833337039", 8);});
+//    }
 
     @AfterEach
     void tearDown() {
