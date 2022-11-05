baca934cec3be5e906473f2ff93c7ad8ae5286dd
Tan Min Han <g00377211@atu.ie>
2022-11-05 12:36:34 +0000

    Changes with removing setter method and put in Constructor to test

--- src/main/java/ie/atu/assignment1/Passenger.java
+++ src/main/java/ie/atu/assignment1/Passenger.java
@@ -5,11 +5,45 @@ public class Passenger {
     private int age;
 
     public Passenger(String title, String name, String id, String phone, int age){
-        this.title = title;
-        this.name =name;
-        this.id = id;
-        this.phone = phone;
-        this.age = age;
+        //Check Title
+        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Ms")){
+            this.title = title;
+        }
+        else{
+            throw new IllegalArgumentException("This is not a valid title");
+        }
+
+        //Check name length
+        if(name.length() >= 3){
+            this.name = name;
+        }
+        else{
+            throw new IllegalArgumentException("Name must be greater than 3 characters");
+        }
+
+        //Check ID length
+        if(id.length() >= 10){
+            this.id = id;
+        }
+        else{
+            throw new IllegalArgumentException("ID length must be greater than 10 characters");
+        }
+
+        //Check Phone Numbers length
+        if(phone.length() >= 7){
+            this.phone = phone;
+        }
+        else{
+            throw new IllegalArgumentException("Phone must be greater than 7 numbers");
+        }
+
+        //Check Age eligibility to fly
+        if(age >= 16){
+            this.age = age;
+        }
+        else{
+            throw new IllegalArgumentException("Age below 16 is too young to fly");
+        }
     }
 
     public String getTitle() {
@@ -32,55 +66,4 @@ public class Passenger {
         return age;
     }
 
-    public void setTitle(String title) {
-        //Check Title
-        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Ms")){
-            this.title = title;
-        }
-        else{
-            throw new IllegalArgumentException("This is not a valid title");
-        }
-
-    }
-
-    public void setName(String name) {
-        //Check name length
-        if(name.length() >= 3){
-            this.name = name;
-        }
-        else{
-            throw new IllegalArgumentException("Name must be greater than 3 characters");
-        }
-
-    }
-
-    public void setId(String id) {
-        //Check ID length
-        if(id.length() >= 10){
-            this.id = id;
-        }
-        else{
-            throw new IllegalArgumentException("ID length must be greater than 10 characters");
-        }
-    }
-
-    public void setPhone(String phone) {
-        //Check Phone Numbers length
-        if(phone.length() >= 7){
-            this.phone = phone;
-        }
-        else{
-            throw new IllegalArgumentException("Phone must be greater than 7 numbers");
-        }
-    }
-
-    public void setAge(int age) {
-        //Check Age eligibility to fly
-        if(age >= 16){
-            this.age = age;
-        }
-        else{
-            throw new IllegalArgumentException("Age below 16 is too young to fly");
-        }
-    }
 }
--- src/test/java/ie/atu/assignment1/PassengerTest.java
+++ src/test/java/ie/atu/assignment1/PassengerTest.java
@@ -12,79 +12,83 @@ class PassengerTest {
     //Setting up the Passenger with null and set up @BeforeEach
     @BeforeEach
     void setUp() {
-        myPassenger = new Passenger(null,null,null,null,0);
     }
 
     //Testing the Title with success case Mr,Mrs,Ms and the fail scenario
     @Test
     void testTitleMr(){
-        myPassenger.setTitle("Mr");
+        myPassenger = new Passenger("Mr","Tan","0037721199","0841412414",22);
         assertEquals("Mr", myPassenger.getTitle());
     }
 
     @Test
     void testTitleMrs(){
-        myPassenger.setTitle("Mrs");
+        myPassenger = new Passenger("Mrs","Tan","0037721199","0841412414",22);
         assertEquals("Mrs", myPassenger.getTitle());
     }
 
     @Test
     void testTitleMs(){
-        myPassenger.setTitle("Ms");
+        myPassenger = new Passenger("Ms","Tan","0037721199","0841412414",22);
         assertEquals("Ms", myPassenger.getTitle());
     }
 
     @Test
     void testTitleFail(){
-        assertThrows(IllegalArgumentException.class, ()-> {myPassenger.setTitle("Mas");});
+        assertThrows(IllegalArgumentException.class, ()-> {myPassenger = new Passenger("Mass","Tan","0037721199","0841412414",22);
+        });
     }
 
     //Testing the Name for at least min 3 characters and a failure with 2 characters
     @Test
     void testName(){
-        myPassenger.setName("Tan");
+        myPassenger = new Passenger("Mr","Tan","0037721199","0841412414",22);
         assertEquals("Tan", myPassenger.getName());
     }
 
     @Test
     void testNameFail(){
-        assertThrows(IllegalArgumentException.class, ()-> {myPassenger.setName("Li");});
+        assertThrows(IllegalArgumentException.class, ()-> {myPassenger = new Passenger("Mr","Li","0037721199","0841412414",22);
+            ;});
     }
 
     //Testing the ID for at least min 10 characters and a failure with less than 10 characters
     @Test
     void testId(){
-        myPassenger.setId("0037721199");
+        myPassenger = new Passenger("Mr","Tan","0037721199","0841412414",22);
         assertEquals("0037721199", myPassenger.getId());
     }
 
     @Test
     void testIdFail(){
-        assertThrows(IllegalArgumentException.class, ()-> {myPassenger.setId("000123");});
+        assertThrows(IllegalArgumentException.class, ()-> {myPassenger = new Passenger("Mr","Tan","00123","0841412414",22);
+        });
     }
 
     //Testing the phone for at least min 7 characters and a failure with less than 7 characters
     @Test
     void testPhone(){
-        myPassenger.setPhone("0833337038");
+        myPassenger = new Passenger("Mr","Tan","0037721199","0833337038",22);
         assertEquals("0833337038", myPassenger.getPhone());
     }
 
     @Test
     void testPhoneFail(){
-        assertThrows(IllegalArgumentException.class, ()-> {myPassenger.setPhone("999");});
+        assertThrows(IllegalArgumentException.class, ()-> {myPassenger = new Passenger("Mr","Tan","0037721199","151515",22);
+        });
     }
 
     //Testing the age for at least min 16 and a failure with younger than 16 years old
     @Test
     void testAge(){
-        myPassenger.setAge(22);
+        myPassenger = new Passenger("Mr","Tan","0037721199","0841412414",22);
         assertEquals(22, myPassenger.getAge());
     }
 
     @Test
     void testAgeFail(){
-        assertThrows(IllegalArgumentException.class, ()-> {myPassenger.setAge(8);});
+        assertThrows(IllegalArgumentException.class, ()-> {myPassenger = new Passenger("Mr","Tan","0037721199","0841412414",8);
+        });
     }
 
     @AfterEach
